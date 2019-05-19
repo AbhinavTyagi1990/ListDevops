51526d6772074cfd1af3b6f387c29e94243c5016
Abhinav Tyagi <tyagi.abhinav1990@gmail.com>
2019-05-19 12:32:34 -0400

    Add Material Design

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html'
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
 })
 
 
 export class PostCreateComponent{
-    newPost = 'Test1';
-    onAddPost() {
-        
+    enteredValue = '';
+    newPost = '';
+
+    onAddPost(postInput: HTMLTextAreaElement) {
+        this.newPost = this.enteredValue;     
     }
 }
